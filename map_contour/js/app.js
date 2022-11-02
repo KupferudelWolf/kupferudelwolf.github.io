82df3515be4bd52b22b363965deb6a5ffcc76c92
Avoyt <wolf.mail95@gmail.com>
2022-11-02 15:21:13 -0400

    Image import.

--- map_contour/js/app.js
+++ map_contour/js/app.js
@@ -10,18 +10,45 @@ import rangeSlider from '../build/rangeslider/rangeslider.js';
     class App {
         constructor() {
             // this.initRangeSliders();
+            this.initControls();
             this.updateCanvas();
             $( window ).on( 'resize', () => {
                 this.onResize();
             } );
         }
 
-        updateCanvas() {
-            const container = $( '.container.canvas' );
-            const aspect_cvs = CVS.width / CVS.height;
-            const aspect_win = container.width() / container.height();
-            const is_vert = aspect_win > aspect_cvs;
-            container.toggleClass( 'vertical', is_vert );
+        initControls() {
+            var double_click = 0;
+            $( '#import' ).on( 'click', () => {
+                $( '#import-file' ).trigger( 'click' );
+            } );
+            $( '#output' ).on( 'click', () => {
+                const delta = Date.now() - double_click;
+                double_click = Date.now();
+                if ( this.state && delta > 200 ) return;
+                console.log( delta );
+                $( '#import-file' ).trigger( 'click' );
+            } );
+            $( '#import-file' ).on( 'change', ( event ) => {
+                this.state = 'loading';
+                const files = event.target.files;
+
+                if ( !files.length ) return;
+
+                const reader = new FileReader();
+                reader.onload = ( e ) => {
+                    const img = new Image();
+                    img.onload = () => {
+                        CVS.width = img.width;
+                        CVS.height = img.height;
+                        CTX.drawImage( img, 0, 0 );
+                        this.updateCanvas();
+                        this.state = 'ready';
+                    };
+                    img.src = e.target.result;
+                };
+                reader.readAsDataURL( files[ 0 ] );
+            } );
         }
 
         initRangeSliders() {
@@ -38,6 +65,14 @@ import rangeSlider from '../build/rangeslider/rangeslider.js';
 
         onResize() {
             this.updateCanvas();
+        }
+
+        updateCanvas() {
+            const container = $( '.container.canvas' );
+            const aspect_cvs = CVS.width / CVS.height;
+            const aspect_win = container.width() / container.height();
+            const is_vert = aspect_win > aspect_cvs;
+            container.toggleClass( 'vertical', is_vert );
         }
     }
 
