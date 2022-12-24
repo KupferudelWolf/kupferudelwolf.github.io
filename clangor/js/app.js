fb139933d592d2d7857b2929f0055aed062c5986
Avoyt <wolf.mail95@gmail.com>
2022-12-24 16:40:02 -0500

    Scaffolding for clangor.

---
+++ clangor/js/app.js
@@ -0,0 +1,17 @@
+/*jshint esversion: 7*/
+
+import AV from '/build/av.module.js/av.module.js';
+
+( function () {
+    //
+
+    class App {
+        constructor() {
+            //
+        }
+    }
+
+    $( function () {
+        const APP = new App();
+    } );
+} )();
