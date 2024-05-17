82f1bb1006462833998c47d3d3d075c0eb2fdf75
Avoyt <wolf.mail95@gmail.com>
2024-05-17 12:14:24 -0400

    PHP version now sorts directories properly.

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>owo</title>
     <style>
-        body, body * {
+        body,
+        body * {
             font-size: x-small;
             text-align: center;
         }
+
         body {
             display: flex;
             align-items: center;
@@ -13,22 +15,26 @@
         }
     </style>
 </head>
+
 <body>
     <div class="container">
         <p>owo what's this?</p>
         <div>
             <?php
-                $sep = '&#x2022;';
-                $match = array( 'index.html', 'index.php' );
-                $dirs = array_filter( glob( '*' ), 'is_dir' );
-                echo $sep;
-                foreach ( $dirs as $d ) {
-                    if ( $d == '_template' ) continue;
-                    $contents = scandir( $d );
-                    if ( empty( array_intersect( $match, $contents ) ) ) continue;
-                    $name = strtolower( str_replace( '_', ' ', $d ) );
-                    echo ' <a href="./' . $d . '">' . $name . '</a> ' . $sep;
-                }
+            $sep = '&#x2022;';
+            $match = array('index.html', 'index.php');
+            $dirs = array_filter(glob('*'), 'is_dir');
+            natcasesort($dirs);
+            echo $sep;
+            foreach ($dirs as $d) {
+                if ($d == '_template')
+                    continue;
+                $contents = scandir($d);
+                if (empty(array_intersect($match, $contents)))
+                    continue;
+                $name = strtolower(str_replace('_', ' ', $d));
+                echo ' <a href="./' . $d . '">' . $name . '</a> ' . $sep;
+            }
             ?>
         </div>
     </div>
